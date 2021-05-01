29473d70ff1d9ac7c36baec63f755d5600612e34
Desquivel501 <3523114870101@ingenieria.usac.edu.gt>
2021-05-01 12:25:38 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1044,6 +1044,11 @@ def agregarFactura():
     
     return jsonify(newObj)
 
+@app.route('/<string:page_name>/')
+def render_static(page_name):
+    return render_template('%s.html' % page_name)
+
+
 
 if __name__ == "__main__":
     app.run(threaded=True, port=3000, debug=True)
